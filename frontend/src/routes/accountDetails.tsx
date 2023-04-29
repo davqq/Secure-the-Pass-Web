b82d96390b200925c332a0c804cf0988d123b721
David <68165812+davqq@users.noreply.github.com>
2023-04-29 21:17:31 +0200

    Update accountDetails.tsx

--- frontend/src/routes/accountDetails.tsx
+++ frontend/src/routes/accountDetails.tsx
@@ -26,7 +26,7 @@ const accountDetails = (props: accountDetailsProps) => {
 
   const handleAdd = () => {
     fetch(env.API_URL + "/updateaccount", {
-      method: "POST",
+      method: "PUT",
       headers: [
         ["Content-Type", "application/json"],
         ["Authorization", `${document.cookie}`],
