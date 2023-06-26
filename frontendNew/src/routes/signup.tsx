9f841c47309ffcdb7d253f509877cea07ab99486
davqq <davidvongarrel10219@gmail.com>
2023-06-26 20:37:11 +0200

    w/e

--- frontendNew/src/routes/signup.tsx
+++ frontendNew/src/routes/signup.tsx
@@ -143,7 +143,7 @@ function RegisterPage() {
             <div>
               <button
                 type="submit"
-                className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+                className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-500 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
               >
                 Register
               </button>
