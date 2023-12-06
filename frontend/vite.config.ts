64a01d4d13e2bd52a275706a045fe524d7208851
davqq <davidvongarrel10219@gmail.com>
2023-12-06 19:30:23 +0100

    change config file

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,7 +5,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "dist",
     rollupOptions: {
       input: {
         main: "/public/index.html",
